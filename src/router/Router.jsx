19d769e2013a13ba93719a8e554a7ce355375e95
PMBPG <pmbpk@yahoo.com>
2024-03-11 14:44:05 +0330

    fix bugs for deploy

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, } from 'react-router-dom';
 import Home from '../pages/Home';
 import BlogPage from '../pages/BlogPage';
 import AuthorPage from '../pages/AuthorPage';
@@ -6,7 +6,7 @@ import AuthorPage from '../pages/AuthorPage';
 function Router() {
   return (
     <Routes>
-        <Route index path="/home" element={<Home />}/>
+        <Route index path="/" element={<Home />}/>
         <Route path='/blogs/:slug' element={<BlogPage />} />
         <Route path='/authors/:slug' element={<AuthorPage />} />
     </Routes>
